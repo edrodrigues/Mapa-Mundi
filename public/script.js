407cd6f8837e13801677126d434f4226f9aa5310
Edmilson Rodrigues <ed.ufpe@gmail.com>
2024-10-05 16:52:09 -0300

    Atualiza CSS e paginas.

---
+++ public/script.js
@@ -0,0 +1,4 @@
+// Add your JavaScript code here
+// You can use this script to enhance the functionality of your website
+// For example, you can add animations, interactive elements, or dynamic content
+// This script will be loaded after the HTML content is parsed
